1061689119085ec752a53c8dbe417e344896ce66
xiaodaxin <gisxx@qq.com>
2017-09-11 19:55:58 +0800

    2017.09.11

--- js/index.js
+++ js/index.js
@@ -1,41 +1,97 @@
-
+var xueyuanData = null;
 //打开菜单
-function openMenu(){
+function openMenu() {
 	$("#openMenu").hide();
-	$("#left-menu ").animate({left:"15px"});
+	$("#left-menu ").animate({
+		left: "15px"
+	});
 }
-
 
 // 隐藏菜单
-function closeMenu(){
-	$("#left-menu ").animate({left:"-50px"},function(){
+function closeMenu() {
+	$("#left-menu ").animate({
+		left: "-50px"
+	}, function() {
 		$("#openMenu").show();
 	});
-	
+
 }
 
+//左下角显示与隐藏
 
-	//左下角显示与隐藏
+var more_btn_sum = 1;
 
-var more_btn_sum=1;
-function openAndCloseMoreBtn(){
-    if(more_btn_sum!=-1){
-    		
-		$('#jiucuo').animate({'left':'10px','bottom':'130px','opacity':'1'});
-		$('#xinzeng').animate({'left':'68px','bottom':'113px','opacity':'1'});
-		$('#shezhi').animate({'left':'114px','bottom':'67px','opacity':'1'});
-		$('#wode').animate({'left':'140px','bottom':'10px','opacity':'1'});
-	}else{
-		$('#jiucuo').animate({'left':'0px','bottom':'0px','opacity':'0'});
-		$('#xinzeng').animate({'left':'0px','bottom':'0px','opacity':'0'});
-		$('#shezhi').animate({'left':'0px','bottom':'0px','opacity':'0'});
-		$('#wode').animate({'left':'0px','bottom':'0px','opacity':'0'});
+function openAndCloseMoreBtn() {
+	if(more_btn_sum != -1) {
+
+		$('#jiucuo').animate({
+			'left': '10px',
+			'bottom': '130px',
+			'opacity': '1'
+		});
+		$('#xinzeng').animate({
+			'left': '68px',
+			'bottom': '113px',
+			'opacity': '1'
+		});
+		$('#shezhi').animate({
+			'left': '114px',
+			'bottom': '67px',
+			'opacity': '1'
+		});
+		$('#wode').animate({
+			'left': '140px',
+			'bottom': '10px',
+			'opacity': '1'
+		});
+	} else {
+		$('#jiucuo').animate({
+			'left': '0px',
+			'bottom': '0px',
+			'opacity': '0'
+		});
+		$('#xinzeng').animate({
+			'left': '0px',
+			'bottom': '0px',
+			'opacity': '0'
+		});
+		$('#shezhi').animate({
+			'left': '0px',
+			'bottom': '0px',
+			'opacity': '0'
+		});
+		$('#wode').animate({
+			'left': '0px',
+			'bottom': '0px',
+			'opacity': '0'
+		});
 	}
-	more_btn_sum= more_btn_sum*-1;
+	more_btn_sum = more_btn_sum * -1;
 }
+
 
 // 打开搜索页面
-function openSearchDiv(){
+function openSearchDiv() {
 	$("#searchDiv").show();
-	$("#search-input").focus();
+	// $("#search-input").focus();
+	if(xueyuanData != null) {
+		console.log("lll")
+		return;
+	} else {
+		
+		mui.get("http://api.map.baidu.com/geodata/v3/poi/list?geotable_id=175814&ak=WtSZiDjE0KEgfSMsxGY7pxng", function(data) {
+			xueyuanData = data.pois;
+			console.log(JSON.stringify(xueyuanData))
+			var li=[];
+			for(var a=0;a<xueyuanData.length;a++){
+				li[a]=$('<li data-index="'+xueyuanData[a].title+'" class="mui-table-view-cell"> <div>	<img src="fonts/xueyuan.svg" ></div><font>'+xueyuanData[a].title+'</font></li>')
+				$(li[a]).click(function(){
+					console.log($(this).html());
+					console.log($(this).attr("data-index"));
+				});
+				$("#pois").append(li[a]);
+			}
+		}, "json")
+	}
+	
 }
--- js/search.js
+++ js/search.js
@@ -1,3 +1,20 @@
+//区域滑动初始化
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
 function returnMap(){
     $("#searchDiv").hide();
 }
+$('#choiceTyoe li').click(function(e){
+	console.log(e.id);
+	$(this).siblings().children("a").removeClass("a-clicked");
+	$(this).children("a").addClass("a-clicked");
+	
+	
+	mui.get("http://api.map.baidu.com/geodata/v3/poi/list?geotable_id=175814&ak=WtSZiDjE0KEgfSMsxGY7pxng",function(data){
+		
+	},"json")
+})
+
+
